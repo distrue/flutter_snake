b10b3b7715ce1d85ddd50e57d7d65d49644d575b
Y.Jung <gilsang9808@gmail.com>
2020-01-21 16:38:01 +0900

    flutter project initialization

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/distrue/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/distrue/Desktop/SourceTree/snakegame"
+export "FLUTTER_TARGET=/Users/distrue/Desktop/SourceTree/snakegame/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/distrue/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
